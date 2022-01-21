3a99c4e2e64974791e7b831b282419c49ccc1e29
Adubbz <Adubbz@users.noreply.github.com>
2022-01-21 21:07:42 +1100

    Changed from overwriting createLevel to a head inject

--- Forge/src/main/java/terrablender/mixin/client/MixinMinecraft.java
+++ Forge/src/main/java/terrablender/mixin/client/MixinMinecraft.java
@@ -27,8 +27,10 @@ import net.minecraft.world.level.levelgen.WorldGenSettings;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.WorldData;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import terrablender.hooks.MinecraftHooks;
 
 import java.util.function.Function;
@@ -39,13 +41,14 @@ public abstract class MixinMinecraft
     @Shadow
     private void doLoadLevel(String p_91220_, RegistryAccess.RegistryHolder p_91221_, Function<LevelStorageSource.LevelStorageAccess, DataPackConfig> p_91222_, Function4<LevelStorageSource.LevelStorageAccess, RegistryAccess.RegistryHolder, ResourceManager, DataPackConfig, WorldData> p_91223_, boolean p_91224_, Minecraft.ExperimentalDialogType p_91225_, boolean creating) {}
 
-    @Overwrite
-    public void createLevel(String levelName, LevelSettings levelSettings, RegistryAccess.RegistryHolder registryAccess, WorldGenSettings currentSettings)
+    @Inject(method = "createLevel", at = @At("HEAD"), cancellable = true)
+    public void createLevel(String levelName, LevelSettings levelSettings, RegistryAccess.RegistryHolder registryAccess, WorldGenSettings currentSettings, CallbackInfo ci)
     {
         this.doLoadLevel(levelName, registryAccess, (levelStorageAccess) -> {
             return levelSettings.getDataPackConfig();
         }, (levelStorageAccess, p_167887_, resourceManager, dataPackConfig) -> {
             return MinecraftHooks.createPrimaryLevelData(levelSettings, registryAccess, currentSettings, resourceManager);
         }, false, Minecraft.ExperimentalDialogType.NONE, true);
+        ci.cancel();
     }
 }
