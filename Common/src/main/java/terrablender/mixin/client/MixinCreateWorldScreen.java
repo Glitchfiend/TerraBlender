b93322a97249be95d9e6594f364073e9276f76a7
Adubbz <Adubbz@users.noreply.github.com>
2022-02-05 11:15:35 +1100

    Fixed crashing when creating a world under certain circumstances. Closes #26

---
+++ Common/src/main/java/terrablender/mixin/client/MixinCreateWorldScreen.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) Glitchfiend
+ * <p>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package terrablender.mixin.client;
+
+import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import terrablender.api.BiomeProviders;
+import terrablender.data.DataPackManager;
+
+@Mixin(CreateWorldScreen.class)
+public class MixinCreateWorldScreen
+{
+    @Inject(method = "removed", at = @At("HEAD"))
+    public void onRemoved(CallbackInfo ci)
+    {
+        BiomeProviders.remove(DataPackManager.DATA_PACK_PROVIDER_LOCATION);
+    }
+}
