b7d1a70dc87aef254943ae99071ed165eb17c930
modmuss50 <modmuss50@gmail.com>
2022-11-18 18:39:48 +0000

    Inject biome sources before fabric biome modifications. (#96)
    Fixes https://github.com/FabricMC/fabric/issues/2633

--- Fabric/src/main/java/terrablender/mixin/MixinMinecraftServer.java
+++ Fabric/src/main/java/terrablender/mixin/MixinMinecraftServer.java
@@ -5,20 +5,19 @@
 package terrablender.mixin;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.progress.ChunkProgressListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import terrablender.util.LevelUtils;
 
-@Mixin(MinecraftServer.class)
+// NOTE: Inject before fabric biome modifications (priority 1000)
+// but after fabric has modified the nether biome source (priority 990).
+@Mixin(value = MinecraftServer.class, priority = 995)
 public class MixinMinecraftServer
 {
-    // NOTE: We can't inject at HEAD because we're trying to inject after Fabric does.
-    // Fabric nukes our initialization if we are called before it.
-    @Inject(method = "createLevels", at = @At(value = "FIELD", shift = At.Shift.BEFORE, ordinal = 0), require = 1)
-    private void onCreateLevels(ChunkProgressListener listener, CallbackInfo ci)
+    @Inject(method = "<init>", at = @At("RETURN"), require = 1)
+    private void onInit(CallbackInfo ci)
     {
         LevelUtils.initializeOnServerStart((MinecraftServer)(Object)this);
     }
