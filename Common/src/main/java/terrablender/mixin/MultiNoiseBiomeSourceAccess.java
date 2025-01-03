c66c0355031a69dca228f4d40f08069f5b5a756c
ishland <ishlandmc@yeah.net>
2025-01-03 19:17:50 +0800

    change: create clean cache when generating biomes

---
+++ Common/src/main/java/terrablender/mixin/MultiNoiseBiomeSourceAccess.java
@@ -0,0 +1,20 @@
+package terrablender.mixin;
+
+import com.mojang.datafixers.util.Either;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Climate;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSourceParameterList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MultiNoiseBiomeSource.class)
+public interface MultiNoiseBiomeSourceAccess {
+
+    @Mutable
+    @Accessor
+    void setParameters(Either<Climate.ParameterList<Holder<Biome>>, Holder<MultiNoiseBiomeSourceParameterList>> parameters);
+
+}
