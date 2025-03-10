085ea122bb7317f0af9e538b4d05ebdb4e0db51f
VoidLeech <57987812+VoidLeech@users.noreply.github.com>
2025-03-10 20:14:57 +0100

    1.20.1 End Backport (#204)
    * Revert "Revert "Backport Support for The End (#164)""
    
    This reverts commit 067e8c8435b734bbf5515c02c1ebb42393ea6c20.
    
    * build: pin vanillagradle version
    
    none of the common dependencies were being located, fsr

--- Common/src/main/java/terrablender/mixin/MixinNoiseGeneratorSettings.java
+++ Common/src/main/java/terrablender/mixin/MixinNoiseGeneratorSettings.java
@@ -21,6 +21,7 @@ import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -35,30 +36,26 @@ public class MixinNoiseGeneratorSettings implements IExtendedNoiseGeneratorSetti
     @Shadow
     private SurfaceRules.RuleSource surfaceRule;
 
-    private RegionType regionType = null;
+    @Unique
+    private SurfaceRuleManager.RuleCategory ruleCategory = null;
+    @Unique
     private SurfaceRules.RuleSource namespacedSurfaceRuleSource = null;
 
     @Inject(method = "surfaceRule", at = @At("HEAD"), cancellable = true)
     private void surfaceRule(CallbackInfoReturnable<SurfaceRules.RuleSource> cir)
     {
-        if (this.regionType != null)
+        if (this.ruleCategory != null)
         {
             if (this.namespacedSurfaceRuleSource == null)
-                this.namespacedSurfaceRuleSource = regionType == RegionType.NETHER ? SurfaceRuleManager.getNamespacedRules(SurfaceRuleManager.RuleCategory.NETHER, this.surfaceRule) : SurfaceRuleManager.getNamespacedRules(SurfaceRuleManager.RuleCategory.OVERWORLD, this.surfaceRule);
+                this.namespacedSurfaceRuleSource = SurfaceRuleManager.getNamespacedRules(this.ruleCategory, this.surfaceRule);
 
             cir.setReturnValue(this.namespacedSurfaceRuleSource);
         }
     }
 
     @Override
-    public void setRegionType(RegionType regionType)
+    public void setRuleCategory(SurfaceRuleManager.RuleCategory ruleCategory)
     {
-        this.regionType = regionType;
-    }
-
-    @Override
-    public RegionType getRegionType()
-    {
-        return this.regionType;
+        this.ruleCategory = ruleCategory;
     }
 }
