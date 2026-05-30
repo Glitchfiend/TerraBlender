3645adedb501f377ff4f4a5e0859e7c61fb056c8
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-05-29 20:34:51 -0400

    addBiomeSimilar Helper Method

--- common/src/main/java/terrablender/api/BiomeMapping.java
+++ common/src/main/java/terrablender/api/BiomeMapping.java
@@ -1,11 +1,15 @@
 package terrablender.api;
 
+import com.google.common.collect.ImmutableList;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate;
+import terrablender.worldgen.RegionUtils;
+
+import java.util.List;
 
 public record BiomeMapping(Climate.ParameterPoint parameters, ResourceKey<Biome> biome)
 {
@@ -14,4 +18,10 @@ public record BiomeMapping(Climate.ParameterPoint parameters, ResourceKey<Biome>
                     Climate.ParameterPoint.CODEC.fieldOf("parameters").forGetter(BiomeMapping::parameters),
                     ResourceKey.codec(Registries.BIOME).fieldOf("biome").forGetter(BiomeMapping::biome)
             ).apply(instance, BiomeMapping::new));
+
+    public static List<BiomeMapping> addBiomeSimilar(ResourceKey<Biome> similarVanillaBiome, ResourceKey<Biome> biome)
+    {
+        List<Climate.ParameterPoint> points = RegionUtils.getVanillaParameterPoints(similarVanillaBiome).stream().collect(ImmutableList.toImmutableList());
+        return points.stream().map(point -> new BiomeMapping(point, biome)).collect(ImmutableList.toImmutableList());
+    }
 }
